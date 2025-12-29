a8bd04dc8cd855e98b580f78fcb84e7c0ed5b1fe
Agaslez <agataslezak7@gmail.com>
2025-12-29 16:14:36 +0100

    fix: disable Facebook Pixel until real ID is configured

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Container } from './layout/Container';
 import { Section } from './layout/Section';
 
